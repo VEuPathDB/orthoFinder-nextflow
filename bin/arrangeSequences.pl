e9c743dade1266240b55e6e2a7d6f24db166d4ab
rdemko2332 <rdemko2332@gmail.com>
2023-06-09 15:13:07 -0400

    Guarantee fasta is 1 line per seq

--- bin/arrangeSequences.pl
+++ bin/arrangeSequences.pl
@@ -11,28 +11,50 @@ my ($input,$output);
 
 open(my $data, '<', $input) || die "Could not open file $input: $!";
 
+my $firstDefLine = 0;
+open(TEMP,">>./temp_$input");
+while (my $line = <$data>) {
+    chomp $line;
+    if ($line =~ /^>/ && $firstDefLine == 0) {
+	print TEMP "$line\n";
+	$firstDefLine+=1;
+    }
+    elsif ($line =~ /^>/ && $firstDefLine > 0) {
+	print TEMP "\n$line\n";
+    }
+    else {
+	print TEMP "$line";
+    }
+}
+
+close $data;
+close TEMP;
+
+open(my $fixed, '<', "temp_$input");
+
 my ($currentOrg, $organism, $currentSeq, $sequence);
 my $foundAAs = 0;
 
 open(HOLD,">>./holdFile");
-while (my $line = <$data>) {
+while (my $line = <$fixed>) {
+    chomp $line;
     if ($line =~ /^>/) {
 	$currentOrg = $line;
     }
     elsif ($line =~  /[EFILPQ]/ && $foundAAs == 0) {
 	open(OUT,">./$output");
-        print OUT "$currentOrg$line";
+        print OUT "$currentOrg\n$line\n";
         close OUT;
 	$foundAAs += 1;
     }
     else {
-	print HOLD "$currentOrg$line";
+	print HOLD "$currentOrg\n$line\n";
     }
 }	
 
 die "Fasta file $input does not contain necessary Amino Acids" unless ($foundAAs == 1);
 
-close $data;
+close $fixed;
 close HOLD;
 
 system("cat ./holdFile >> ./$output");
