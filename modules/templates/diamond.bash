bea4791099a0d53507a2a82552869b3d2e896f48
rdemko2332 <rdemko2332@gmail.com>
2023-08-07 12:01:09 -0400

    Changing arguments for diamond job

--- modules/templates/diamond.bash
+++ modules/templates/diamond.bash
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-perl /usr/bin/orthoFinderBlast.pl --database ${pair[0]} --query ${pair[1]} --sequenceIDs SequenceIDs.txt --speciesIDs SpeciesIDs.txt
+perl /usr/bin/orthoFinderBlast.pl --database ${pair[0]} --query ${pair[1]} --sequences_id_file SequenceIDs.txt --species_id_file SpeciesIDs.txt
 
 cp Blast*.txt.gz hold.txt.gz
 gunzip hold.txt.gz
