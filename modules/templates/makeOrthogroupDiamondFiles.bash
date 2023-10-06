2174cfa2b984b17c730a03290630ebd0cb850943
rdemko2332 <rdemko2332@gmail.com>
2023-10-06 14:03:14 -0400

    Resolving issue so blast results between different organisms are included

--- modules/templates/makeOrthogroupDiamondFiles.bash
+++ modules/templates/makeOrthogroupDiamondFiles.bash
@@ -7,17 +7,19 @@ SPECIES_0_ORTHOLOGS=${target}.orthologs
 for query in ${queries.join(' ')}; do
     SPECIES_1_ORTHOLOGS=\$query.orthologs
 
-    DIAMOND_FILE=Blast\${query}_${target}.txt
+    DIAMOND_FILE=Blast${target}_\${query}.txt
     OUTPUT_FILE=OrthologGroup_Blast\${query}_${target}.txt
 
-
-
-    makeOrthologGroupDiamondFiles.pl --species0 \$SPECIES_0_ORTHOLOGS --species1 \$SPECIES_1_ORTHOLOGS --diamondFile \$DIAMOND_FILE --outputFile \$OUTPUT_FILE;
+    makeOrthologGroupDiamondFiles.pl --species0 \$SPECIES_0_ORTHOLOGS \
+				     --species1 \$SPECIES_1_ORTHOLOGS \
+				     --diamondFile \$DIAMOND_FILE \
+				     --outputFile \$OUTPUT_FILE;
     sort \$OUTPUT_FILE >\$OUTPUT_FILE.sorted
     rm \$OUTPUT_FILE
 
     # NOTE:  This script will append to OG files in this directory:   OGXXXXX.sim
-    splitBlastsIntoGroupsFiles.pl --input_file \$OUTPUT_FILE.sorted --output_file_suffix ".sim"
+    splitBlastsIntoGroupsFiles.pl --input_file \$OUTPUT_FILE.sorted \
+				  --output_file_suffix ".sim"
 done;
 
 # NOTE: in real workflow, this will never be needed but for tests we need to always provide output files
