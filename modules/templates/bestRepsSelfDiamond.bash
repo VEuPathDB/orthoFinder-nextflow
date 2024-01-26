8e897e7f1b44c44d5f75ad7a8f70e039a8eb1ec9
rdemko2332 <rdemko2332@gmail.com>
2024-01-26 15:21:26 -0500

    Reverting evalue parameter for bestrepselfdiamond

--- modules/templates/bestRepsSelfDiamond.bash
+++ modules/templates/bestRepsSelfDiamond.bash
@@ -7,7 +7,7 @@ diamond blastp \
   -d newdb.dmnd \
   -q $bestRepSubset \
   -o bestReps.out \
-  -e 0.00000000000000000001 \
+  -e 0.00001 \
   -f 6 $params.bestRepDiamondOutputFields \
   --comp-based-stats 0 \
   --no-self-hits
