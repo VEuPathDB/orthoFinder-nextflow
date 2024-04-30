74d43c522c336166bdcd527c8d974e2f4fa949a8
rdemko2332 <rdemko2332@gmail.com>
2024-04-30 17:12:10 -0400

    Resolving syntax issue with create gene trees

--- modules/templates/createGeneTrees.bash
+++ modules/templates/createGeneTrees.bash
@@ -7,7 +7,7 @@ do
     SEQ_COUNT=\$(grep ">" \$f | wc -l) \
         && echo "\$f" \
 	&& echo "\$SEQ_COUNT"
-    if [ \$SEQ_COUNT >= 3 ]; then
+    if [ "\$SEQ_COUNT" -ge 3 ]; then
 	mafft --auto --anysymbol \$f | fasttree -mlnni 4 > \$f.tree
     else
 	touch \$f.tree
