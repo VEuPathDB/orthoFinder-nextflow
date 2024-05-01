838282f2d3a5b91fce46525c5c133a66700e94b7
rdemko2332 <rdemko2332@gmail.com>
2024-05-01 18:53:17 -0400

    Changing template to restart gene tree jobs

--- modules/templates/createGeneTrees.bash
+++ modules/templates/createGeneTrees.bash
@@ -5,8 +5,7 @@ set -euo pipefail
 for f in *.fasta;
 do
     SEQ_COUNT=\$(grep ">" \$f | wc -l) \
-        && echo "\$f" \
-	&& echo "\$SEQ_COUNT"
+        && echo "\$f"
     if [ "\$SEQ_COUNT" -ge 3 ]; then
 	mafft --auto --anysymbol \$f | fasttree -mlnni 4 > \$f.tree
     else
