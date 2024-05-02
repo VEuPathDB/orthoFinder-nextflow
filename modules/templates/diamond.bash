5f52af4bc71e2aa149ad89523e408d40569561d1
rdemko2332 <rdemko2332@gmail.com>
2024-05-02 19:41:48 -0400

    Reformatting

--- modules/templates/diamond.bash
+++ modules/templates/diamond.bash
@@ -3,16 +3,12 @@
 set -euo pipefail
 
 for query in ${queries.join(' ')}; do
-
     BLAST_FILE=$mappedBlastCache/Blast\${query}_${target}.txt
     if [ -f "\$BLAST_FILE" ]; then
         echo "Taking from Cache for \$BLAST_FILE"
         ln -s \$BLAST_FILE .
     else
-        # TODO:  Review the command line options here!
-        #  TODO:  Move as much as possible here to nextflow.config
         echo "Running Diamond to generate Blast\${query}_${target}.txt"
-
         diamond blastp --ignore-warnings \
 		-d ${orthofinderWorkingDir}/diamondDBSpecies${target}.dmnd \
 		-q ${orthofinderWorkingDir}/Species\${query}.fa \
@@ -22,5 +18,4 @@ for query in ${queries.join(' ')}; do
 		-p 1 \
 		--quiet
     fi
-
 done
