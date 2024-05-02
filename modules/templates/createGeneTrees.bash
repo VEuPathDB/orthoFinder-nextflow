c9cecbc98591df6d5a5ba01c881cb34aba1899ee
rdemko2332 <rdemko2332@gmail.com>
2024-05-02 19:22:04 -0400

    Reformatting

--- modules/templates/createGeneTrees.bash
+++ modules/templates/createGeneTrees.bash
@@ -4,8 +4,7 @@ set -euo pipefail
 
 for f in *.fasta;
 do
-    SEQ_COUNT=\$(grep ">" \$f | wc -l) \
-        && echo "\$f"
+    SEQ_COUNT=\$(grep ">" \$f | wc -l) 
     if [ "\$SEQ_COUNT" -ge 3 ]; then
 	mafft --auto --anysymbol \$f | fasttree -mlnni 4 > \$f.tree
     else
